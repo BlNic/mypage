fccc7bb3309144b492bb4ed53a5e5339aafb426f
Blanka Nicman <bjnicman@o2.pl>
2022-12-30 15:58:36 +0100

    js cleaning509

--- js/script.js
+++ js/script.js
@@ -2,28 +2,25 @@ const welcome = () => {
     console.log("Dopiero się uczę, jest Git");
 }
 
+const abracadabra = document.querySelector(".safety__button");
+const picture = document.querySelector(".safety__image")
 
-const removeImage = () => {
-    const safetyButton = document.querySelector(".safety__button");
-    const picture = document.querySelector(".safety__image")
+abracadabra.addEventListener("click", () => {
+    picture.classList.toggle("image__show");
 
-    safetyButton.innerText =
+    abracadabra.innerText =
         picture.classList.contains("image__show")
             ? "Cudnie!"
             : "Pokaż zdjęcie";
-}
+});
 
-const init = () => {
-    safetyButton.addEventListener("click", removeImage);
-
-    welcome();
-}
+welcome();
 
 /* kalkulator BMI
 
 const heightElement = document.querySelector(".jsHeight");
-let weightElement = document.querySelector(".jsWeight");
-let jsForm = document.querySelector(".jsForm");
+const weightElement = document.querySelector(".jsWeight");
+const jsForm = document.querySelector(".jsForm");
 const jsBmi = document.querySelector(".jsBmi");
 
 jsForm.addEventListener("submit", (event) => {
